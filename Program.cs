9b8d726fc9b52e510f890cb9eaa8251454b3ef9b
anikd <anikd@DESKTOP-B632J11>
2022-04-22 16:47:11 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bike
+{
+    public abstract class Bike
+    {
+        private string model { get; set; }
+        public abstract void Display();
+        
+}
+    public class Honda:Bike
+    {
+        public int speed { get; set; } = 160;
+        private string model { get; set; } = "Honda";
+        public override void Display() 
+        {
+            Console.WriteLine(model);
+        }
+    }
+    public class Honda4 : Honda
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //  Bike bike = new Bike();
+            Honda bike1 = new Honda();
+            bike1.Display();
+            Console.WriteLine(bike1.speed);
+        }
+    }
+}
